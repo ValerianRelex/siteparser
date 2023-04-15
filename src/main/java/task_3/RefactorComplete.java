a20c10c382804f72fed9d5c0412bf7ff223c8bd1
valeriy_panafidin <valeriy_panafidin@relex.ru>
2023-04-15 18:10:41 +0900

    feat: refactor task_3

--- src/main/java/task_3/RefactorComplete.java
+++ src/main/java/task_3/RefactorComplete.java
@@ -1,56 +1,42 @@
-//package task_3;
-//
-//import java.net.InetSocketAddress;
-//
-//public class RefactorComplete {
-//    void processTask(ChannelHandlerContext context) {
-//	InetSocketAddress lineAddress = new InetSocketAddress(getIpAddress(), getUdpPort());
-//
-//	for (Command currentCommand : getAllCommands()) {
-//	    if (currentCommand.getCommandType() == CommandType.REBOOT_CHANNEL) {
-//		if (!currentCommand.isAttemptsNumberExhausted() && currentCommand.isTimeToSend()) {
-//			// Тип переменной и версия Java неизвестны. Предположу что версия Java 10 или выше.
-//			// Иначе нужно точно знать возвращаемый тип, судя по методу - это может быть String.
-//			var commandText = currentCommand.getCommandText();
-//			sendCommandToContext(context, lineAddress, commandText);
-//
-//			try {
-//			    DblIncomeUssdMessage dblIncomeUssdMessage = new DblIncomeUssdMessage(
-//					    lineAddress.getHostName(), lineAddress.getPort(), 0,
-//					    EnumGoIp.getByModel(getGoIpModel()), commandText);
-//
-//			    AdminController.getInstance().processUssdMessage(dblIncomeUssdMessage, false);
-//			} catch (Exception ignored) {
-//			}
-//			currentCommand.setSendDate(new Date());
-//			currentCommand.incSendCounter();
-//			Log.ussd.write(String.format("sent: ip: %s; порт: %d; %s", lineAddress.getHostString(),
-//					lineAddress.getPort(), commandText));
-//		    }
-//		} else {
-//		    deleteCommand(currentCommand.getCommandType());
-//		}
-//	    } else {
-//		if (!currentCommand.isAttemptsNumberExhausted()) {
-//		    var commandText = currentCommand.getCommandText();
-//		    sendCommandToContext(context, lineAddress, commandText);
-//
-//		    try {
-//			DblIncomeUssdMessage dblIncomeUssdMessage = new DblIncomeUssdMessage(lineAddress.getHostName(),
-//					lineAddress.getPort(), 0, EnumGoIp.getByModel(getGoIpModel()), commandText);
-//
-//			AdminController.getInstance().processUssdMessage(dblIncomeUssdMessage, false);
-//		    } catch (Exception ignored) {
-//		    }
-//		    currentCommand.setSendDate(new Date());
-//		    currentCommand.incSendCounter();
-//		    Log.ussd.write(String.format("sent: ip: %s; порт: %d; %s", lineAddress.getHostString(),
-//				    lineAddress.getPort(), commandText));
-//		} else {
-//		    deleteCommand(currentCommand.getCommandType());
-//		}
-//	    }
-//	}
-//	sendKeepAliveOkAndFlush(context);
-//    }
-//}
+package task_3;
+
+import java.net.InetSocketAddress;
+
+public class RefactorComplete {
+    void processTask(ChannelHandlerContext ctx) {
+	InetSocketAddress lineAddress = new InetSocketAddress(getIpAddress(), getUdpPort());
+	CommandType typeToRemove;
+	for (Command currentCommand : getAllCommands()) {
+	    if (currentCommand.getCommandType() == CommandType.REBOOT_CHANNEL) {
+		if (!currentCommand.isAttemptsNumberExhausted() && currentCommand.isTimeToSend()) {
+		    sendCommand(ctx, lineAddress, currentCommand);
+		} else {
+		    typeToRemove = currentCommand.getCommandType();
+		    deleteCommand(typeToRemove);
+		}
+	    } else {
+		if (!currentCommand.isAttemptsNumberExhausted()) {
+		    sendCommand(ctx, lineAddress, currentCommand);
+		} else {
+		    typeToRemove = currentCommand.getCommandType();
+		    deleteCommand(typeToRemove);
+		}
+	    }
+	}
+	sendKeepAliveOkAndFlush(ctx);
+    }
+
+    private void sendCommand(ChannelHandlerContext ctx, InetSocketAddress lineAddress, Command command) {
+	sendCommandToContext(ctx, lineAddress, command.getCommandText());
+	try {
+	    AdminController.getInstance().processUssdMessage(
+			    new DblIncomeUssdMessage(lineAddress.getHostName(), lineAddress.getPort(), 0,
+					    EnumGoip.getByModel(getGoipModel()), command.getCommandText()), false);
+	} catch (Exception ignored) {
+	}
+	command.setSendDate(new Date());
+	Log.ussd.write(String.format("sent: ip: %s; порт: %d; %s", lineAddress.getHostString(), lineAddress.getPort(),
+			command.getCommandText()));
+	command.incSendCounter();
+    }
+}
