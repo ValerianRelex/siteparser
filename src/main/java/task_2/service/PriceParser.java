ec9a510bce03712c68358e2055174d8394293228
valeriy_panafidin <valeriy_panafidin@relex.ru>
2023-04-17 22:04:10 +0900

    feat: work with json string output...

--- src/main/java/task_2/service/PriceParser.java
+++ src/main/java/task_2/service/PriceParser.java
@@ -51,19 +51,19 @@ public class PriceParser {
     public String parse() {
 	try {
 	    final HtmlPage startPage = webClient.getPage(URL_FOR_PARSING);
-	    final Map<String, Map<String, String>> pricesList = getPricesListForAllCountry(startPage);
-	    final String valueAsString = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(pricesList);
+	    final Map<String, Map<String, Integer>> pricesList = getPricesListForAllCountry(startPage);
+	    final String jsonString = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(pricesList);
 	    // TODO: оставить вывод в консольку, пока не реализую вывод с помощью JSP
-	    System.out.println(valueAsString);
-	    return valueAsString;
+	    System.out.println(jsonString);
+	    return jsonString;
 	} catch (IOException e) {
 	    System.err.println(e.getMessage());
 	    return e.getMessage();
 	}
     }
 
-    private Map<String, Map<String, String>> getPricesListForAllCountry(HtmlPage startPage) {
-	Map<String, Map<String, String>> pricesList = new HashMap<>();
+    private Map<String, Map<String, Integer>> getPricesListForAllCountry(HtmlPage startPage) {
+	Map<String, Map<String, Integer>> pricesList = new HashMap<>();
 	List<HtmlDivision> allCountryPath = startPage.getByXPath(XPATH_COUNTRY_BLOCK);
 
 	allCountryPath.forEach(countryPath -> {
@@ -72,7 +72,7 @@ public class PriceParser {
 		HtmlPage countryPage = countryLink.click();
 		// TODO: может стоит использовать класс Optional для обработки ситуаций с NULL !?
 		String countryName = getCountryName(countryPage);
-		Map<String, String> priceList = getPriceList(countryPage);
+		Map<String, Integer> priceList = getPriceList(countryPage);
 		pricesList.put(countryName, priceList);
 		countryPage.remove();
 	    } catch (IOException e) {
@@ -82,9 +82,9 @@ public class PriceParser {
 	return pricesList;
     }
 
-    private Map<String, String> getPriceList(HtmlPage page) {
+    private Map<String, Integer> getPriceList(HtmlPage page) {
 	final List<HtmlDivision> serviceBlock = page.getByXPath(XPATH_PRICE_LIST_BLOCK);
-	final Map<String, String> priceList = new HashMap<>();
+	final Map<String, Integer> priceList = new HashMap<>();
 	final String[] serviceName = new String[1];
 
 	serviceBlock.forEach(htmlDivision -> {
@@ -95,7 +95,7 @@ public class PriceParser {
 		    serviceName[0] = element.getVisibleText();
 		}
 		if ("price-text".equalsIgnoreCase(key)) {
-		    priceList.put(serviceName[0], element.getVisibleText());
+		    priceList.put(serviceName[0], Integer.valueOf(element.getVisibleText()));
 		}
 	    });
 	});
